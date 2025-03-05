b591d6e62a4b42ec5ed28d5e7842aa8c2217bb6f
shengfenn <b614110014@tmu.edu.tw>
2025-03-05 11:50:44 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-print('hello')
-smallest = None
-for value in [12,45,90,67,23,10,38,89,90,38]:
-  if smallest is None:
-    smallest = value
-  elif value<smallest:
-    smallest = value
-print(smallest)
